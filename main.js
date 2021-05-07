df29fceee46a0e70d49b530bf525a8ce4874847f
Nopanun Laochunhanun <nopanun@live.com>
2021-05-08 01:46:14 +0700

    misspelling words

--- main.js
+++ main.js
@@ -74,10 +74,10 @@ function isPerfectSquare(x) {
   return s * s == x;
 }
 
-// Returns true if n is a Fibinacci Number, else false
+// Returns true if n is a Fibonacci Number, else false
 function isFibonacci(n) {
-  // n is Fibinacci if one of 5*n*n + 4 or 5*n*n - 4 or both
-  // is a perferct square
+  // n is Fibonacci if one of 5*n*n + 4 or 5*n*n - 4 or both
+  // is a perfect square
   return isPerfectSquare(5 * n * n + 4) || isPerfectSquare(5 * n * n - 4);
 }
 
